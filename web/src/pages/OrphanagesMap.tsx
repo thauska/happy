d6a7f76ae3243a8e920dea3b756cba57354f330b
Thayana Mamoré <thayanacmamore@gmail.com>
2020-10-13 01:26:44 -0300

    [feat]: Incluído mapa com leaflet e exemplo de tylelayer do openstreetmap e mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarherImg from '../images/map-marker.svg';
 
@@ -23,7 +26,17 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map 
+                center={[-1.4080478,-48.468732]}
+                zoom={17}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+
+                <TileLayer
+                    url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+                />
+            </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
